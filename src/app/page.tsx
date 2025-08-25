5caebc2b7efc8048d00bdf4f69178875db4975eb
Woranon Sansuksavasdi <woranon.san@wdc.com>
2025-08-25 09:38:18 +0700

    fix get start button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,12 @@ export default function Home() {
   const username = useReadLocalStorage<string>("wd_ar_workshop");
 
   const next_link = useMemo(() => {
-    if (username) {
-      return "/collection";
-    } else {
-      return "/register";
-    }
+    return username ? "/collection" : "/register";
   }, [username]);
 
   return (
-    <div className="p-4 flex flex-col w-full h-screen justify-center">
+    <div className="relative p-4 flex flex-col w-full h-screen justify-center">
+      {/* Carousel */}
       <AnimatedContent
         distance={150}
         direction="horizontal"
@@ -35,11 +32,24 @@ export default function Home() {
         <HomeCarousel />
       </AnimatedContent>
 
-      <div className="flex justify-end pr-1 mt-4">
-        <Button asChild>
-          <Link href={next_link}>Getting Started</Link>
-        </Button>
-      </div>
+      {/* ปุ่ม */}
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        duration={0.75}
+        ease="power3.out"
+        initialOpacity={0}
+        animateOpacity
+        threshold={0.2}
+        delay={0.2}
+      >
+        <div className="flex justify-end pr-1 mt-4 relative z-50">
+          <Button asChild>
+            <Link href={next_link}>Getting Started</Link>
+          </Button>
+        </div>
+      </AnimatedContent>
     </div>
   );
 }
