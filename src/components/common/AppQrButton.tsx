ad8819d107483573818efca040b9f7c09a72c5ca
Woranon Sansuksavasdi <woranon.san@wdc.com>
2025-08-25 20:05:48 +0700

    fix old andriod not found scan qr button

--- src/components/common/AppQrButton.tsx
+++ src/components/common/AppQrButton.tsx
@@ -1,14 +1,17 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
 import { QrCodeIcon } from "lucide-react";
 
 export const AppQrButton = () => {
+  const router = useRouter();
+
   return (
-    <Link
-      href="/ar"
-      className="flex flex-col items-center bg-secondary/40 rounded-md border py-4"
+    <button
+      onClick={() => router.push("/ar")}
+      className="flex flex-col items-center w-full bg-secondary/40 rounded-md border py-4"
     >
       <QrCodeIcon className="size-10 text-cyan-300" />
       <span className="font-semibold">Scan Qr Code</span>
-    </Link>
+    </button>
   );
 };
