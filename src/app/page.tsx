bcec076928b8de3bf26bfce306e2373c7d87c726
Woranon Sansuksavasdi <woranon.san@wdc.com>
2025-08-25 09:19:50 +0700

    fix get start button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import Link from "next/link";
-import { useMemo } from "react";
 import { useReadLocalStorage } from "usehooks-ts";
 
 import AnimatedContent from "@/components/blocks/Animations/AnimatedContent/AnimatedContent";
 import { HomeCarousel } from "@/modules/home/components/HomeCarousel";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   const username = useReadLocalStorage<string>("wd_ar_workshop");
-
-  const next_link = useMemo(() => {
-    if (username) {
-      return "/collection";
-    } else {
-      return "/register";
-    }
-  }, [username]);
 
   return (
     <div className="p-4 flex flex-col w-full h-screen justify-center">
@@ -34,13 +26,25 @@ export default function Home() {
       >
         <HomeCarousel />
       </AnimatedContent>
-
-      {/* ปุ่มอยู่นอก AnimatedContent */}
-      <div className="flex justify-end pr-1 mt-4 relative z-50">
-        <Button asChild>
-          <Link href={next_link}>Getting Started</Link>
-        </Button>
-      </div>
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        duration={0.75}
+        ease="power3.out"
+        initialOpacity={0}
+        animateOpacity
+        threshold={0.2}
+        delay={0.2}
+      >
+        <div className="flex justify-end pr-1 mt-4 relative z-[9999]">
+          <Button
+            onClick={() => router.push(username ? "/colleciton" : "register")}
+          >
+            Getting Started
+          </Button>
+        </div>
+      </AnimatedContent>
     </div>
   );
 }
